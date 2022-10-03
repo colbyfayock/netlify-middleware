3c3372c1ef824f0d602c7903ecac3295ec2175e1
colbyfayock <fayock@gmail.com>
2022-10-02 22:15:34 -0400

    error catching

--- middleware.js
+++ middleware.js
@@ -7,10 +7,14 @@ export async function middleware(nextRequest) {
   const request = new MiddlewareRequest(nextRequest);
   const response = await request.next();
 
-  const shipping = shippingRates.find(({ country }) => country === request.geo.country);
+  try {
+    const shipping = shippingRates.find(({ country }) => country === request.geo.country);
 
-  if ( shipping ) {
-    response.setPageProp('shipping', shipping);
+    if ( shipping ) {
+      response.setPageProp('shipping', shipping);
+    }
+  } catch(e) {
+    console.log('e', e);
   }
 
   return response;
