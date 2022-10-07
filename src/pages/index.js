c64b34ffa67e8ca9d45d87f6be960d060fb83a15
@whitep4nth3r <52798353+whitep4nth3r@users.noreply.github.com>
2022-10-07 09:21:18 +0100

    Fix hydration error

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,29 @@
-import Head from 'next/head';
+import Head from "next/head";
 
-import Layout from 'components/Layout';
-import Section from 'components/Section';
-import Container from 'components/Container';
-import Button from 'components/Button';
+import Layout from "components/Layout";
+import Section from "components/Section";
+import Container from "components/Container";
+import Button from "components/Button";
 
-import products from 'data/products.json';
+import * as React from "react";
 
-import styles from 'styles/App.module.scss';
+import products from "data/products.json";
+
+import styles from "styles/App.module.scss";
+
+const useHydrated = () => {
+  const [hydrated, setHydrated] = React.useState(false);
+  React.useEffect(() => {
+    setHydrated(true);
+  }, []);
+  return hydrated;
+};
 
 export default function Home({ shipping }) {
+  const hydrated = useHydrated();
+
   return (
     <Layout>
-
       <Head>
         <title>Space Jelly Shop</title>
         <link rel="icon" href="/favicon.ico" />
@@ -21,16 +32,16 @@ export default function Home({ shipping }) {
       <Section className={styles.homeHeader}>
         <Container>
           <h1 className={styles.homeTitle}>Space Jelly Shop</h1>
-          {shipping.country && (
+          {hydrated && shipping.country && (
             <p className={styles.homeShipping}>
-              { shipping.rate > 0 && (
-                <>{ shipping.rate } Flat Rate Shipping</>
-              )}
-              { shipping.rate === 0 && (
-                <><strong>FREE</strong> Shipping</>
+              {shipping.rate > 0 && <>{shipping.rate} Flat Rate Shipping</>}
+              {shipping.rate === 0 && (
+                <>
+                  <strong>FREE</strong> Shipping
+                </>
               )}
               <br />
-              <span>when shipping to { shipping.country }</span>
+              <span>when shipping to {shipping.country}</span>
             </p>
           )}
         </Container>
@@ -39,22 +50,21 @@ export default function Home({ shipping }) {
       <Section>
         <Container>
           <ul className={styles.productGrid}>
-            {products.map(product => {
+            {products.map((product) => {
               return (
                 <li key={product.id}>
                   <img src={product.image} />
-                  <h2>{ product.name }</h2>
-                  <p>{ product.description }</p>
+                  <h2>{product.name}</h2>
+                  <p>{product.description}</p>
                   <Button>Add to Cart</Button>
                 </li>
-              )
+              );
             })}
           </ul>
         </Container>
       </Section>
-
     </Layout>
-  )
+  );
 }
 
 export async function getStaticProps() {
@@ -62,8 +72,8 @@ export async function getStaticProps() {
     props: {
       shipping: {
         country: null,
-        rate: 99.99
-      }
-    }
-  }
+        rate: 99.99,
+      },
+    },
+  };
 }
